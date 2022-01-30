83ccfb9571f18ead5bffacb636ddffbef8876762
eunji-19 <dmswlc19@gmail.com>
2022-01-30 17:57:27 +0900

    fetch를 이용한 코인사이트

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,172 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useLocation, Routes, Route, Link, useMatch } from "react-router-dom";
+import styled from "styled-components";
+import { InfoData, PriceData } from "../model/CoinModel";
+import Chart from "./Chart";
+import Price from "./Price";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+interface TabProps {
+  readonly isActive: boolean;
+}
+
+const Tab = styled.span<TabProps>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+      props.isActive ? props.theme.accentColor : props.theme.textColor}
+    a {
+    display: block;
+  }
+`;
+
+type RouteParams = {
+  coinId: string;
+};
+
+interface nameState {
+  name: string;
+}
+
+interface RouterState {
+  state: nameState;
+}
+
+function Coin() {
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation() as RouterState;
+
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+
+  const priceMatch = useMatch("/:coinId/price");
+  const chartMatch = useMatch("/:coind/chart");
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })();
+  }, [coinId]);
+
+  return (
+    <Container>
+      <Header>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : info?.name}
+        </Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null ? true : false}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null ? true : false}>
+              <Link to={`/${coinId}/price`}>Prcie</Link>
+            </Tab>
+          </Tabs>
+
+          <Routes>
+            <Route path="/price" element={<Price />} />
+            <Route path="/chart" element={<Chart />} />
+          </Routes>
+        </>
+      )}
+    </Container>
+  );
+}
+
+export default Coin;
